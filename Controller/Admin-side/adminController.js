11a5abc421b5bba02ca79ad3b42f19212d4b4028
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-05-31 16:40:10 +0530

    new commit

--- Controller/Admin-side/adminController.js
+++ Controller/Admin-side/adminController.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const userModel = require("../../Model/userModel")
+const Product = require("../../Model/productModel")
 
 exports.getallUsers = async(req,res)=>{
     try{
@@ -44,4 +45,31 @@ exports.getUserById = async(req,res)=>{
     catch(error){
         res.status(500).json({message:"internal server error",error})
     }
+}
+
+exports.addProduct = async(req,res)=>{
+    try{
+        const {name,description,price,category,gender,stock} = req.body
+        const image = req.file ? req.file.path : null;
+        if(!name||!description||!price){
+            return res.status(400).json({message:"fill all field"})
+        }
+        const newProduct = new Product({
+        name,
+        description,
+        price,
+        category,
+        gender,
+        stock,
+        image:image
+    })
+    await newProduct.save()
+    res.status(201).json({
+        message:"product added succesfull"
+    })
+    }
+    catch(error){
+        res.status(500).json({message:"server error"})
+    }
+ 
 }
--- View/adminRoute.js
+++ View/adminRoute.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const { getallUsers, adminLogin, getUserById } = require("../Controller/Admin-side/adminController")
-
+const { addProduct } = require('../Controller/Admin-side/adminController');
+const upload = require("../middleware/uploadMiddleware")
 const router = express.Router()
 
 router.get("/users",getallUsers)
 router.post("/login",adminLogin)
 router.get("/users/:id",getUserById)
+router.post('/products', upload.single('image'), addProduct);
 
 module.exports=router
