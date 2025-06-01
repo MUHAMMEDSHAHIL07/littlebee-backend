e9521bf362eb8c0da4ea6061936cc1507385eb8d
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-06-01 14:43:19 +0530

    new commit

--- Controller/Admin-side/adminController.js
+++ Controller/Admin-side/adminController.js
@@ -94,3 +94,13 @@ exports.getCategory = async(req,res)=>{
             res.status(500).json({message:"error to get product category"})
         }
 }
+exports.getProductById = async(req,res)=>{
+    try{
+        const product = await Product.findById(req.params.id)
+        if(!product) return res.status(404).json({message:"product not found"})
+        res.status(200).json({product:product})
+    }
+    catch(error){
+        res.status(500).json({message:"server error"})
+    }
+}
