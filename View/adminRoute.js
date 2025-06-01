61c6b311470e3d4d4f74a36a89805f32ee0c26ae
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-06-01 14:44:39 +0530

    new commit

--- View/adminRoute.js
+++ View/adminRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { getallUsers, adminLogin, getUserById, getAllProduct, getCategory } = require("../Controller/Admin-side/adminController")
+const { getallUsers, adminLogin, getUserById, getAllProduct, getCategory, getProductById } = require("../Controller/Admin-side/adminController")
 const { addProduct } = require('../Controller/Admin-side/adminController');
 const upload = require("../middleware/uploadMiddleware")
 const router = express.Router()
@@ -10,4 +10,5 @@ router.get("/users/:id",getUserById)
 router.post('/products', upload.single('image'), addProduct);
 router.get("/allproduct",getAllProduct)
 router.get("/category/:categoryName",getCategory)
+router.get("/product/:id",getProductById)
 module.exports=router
