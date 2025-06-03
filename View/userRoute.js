34a43fe479bb1b2516ad3036720f05e876cf761b
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-06-03 11:37:40 +0530

    new commit

--- View/userRoute.js
+++ View/userRoute.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const {userRegister, userLogin, resetPassword} = require("../Controller/User-side/userController")
 const {allProduct, productId, productCategory} = require("../Controller/User-side/productController")
 const { jwtMiddleware } = require("../middleware/authMiddleware")
-const { addTocart, getCart, removeItem } = require("../Controller/User-side/cartController")
+const { addTocart, getCart, removeItem, buyFromCart } = require("../Controller/User-side/cartController")
 const { Order, getOrder } = require("../Controller/User-side/orderContoller")
 const checkBlock = require("../middleware/checkBlocked")
 
@@ -17,6 +17,7 @@ router.get("/category/:categoryName",productCategory)
 router.post("/addtocart",jwtMiddleware,addTocart)
 router.get("/getCart",jwtMiddleware,getCart)
 router.post("/order",jwtMiddleware,Order)
+router.post("buyfromcart",jwtMiddleware,buyFromCart)
 router.get("/getOrder",jwtMiddleware,getOrder)
 router.delete("/removeItem/:id",jwtMiddleware,removeItem);
 
